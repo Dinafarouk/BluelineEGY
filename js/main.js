77c7a7cd21c58a9d579d75c9c9ec19f79518cc99
Beshoy Mansour <beshoy.r.mansour@gmail.com>
2019-03-10 20:22:37 +0200

    fix UI

--- js/main.js
+++ js/main.js
@@ -256,3 +256,18 @@
     })();
 
 })(jQuery);
+var map = new GMaps({
+    div: '#map',
+    lat: -12.043333,
+    lng: -77.028333
+  });
+  
+  
+  GMaps.geocode({
+    address: 'Egypt',
+    callback: function(results, status){
+      var firstResult = results[0];
+      map.setCenter(firstResult.geometry.location.lat(), firstResult.geometry.location.lng());
+      map.setZoom(7);
+    }
+  }); 
